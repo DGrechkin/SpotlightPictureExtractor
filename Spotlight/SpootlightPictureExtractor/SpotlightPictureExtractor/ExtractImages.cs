7ba15d6131ecddca82b7732e11cf1e0237822b3a
Dmitriy Grechkin <52681778+DGrechkin@users.noreply.github.com>
2019-08-13 22:15:08 -0400

    Add files via upload

---
+++ Spotlight/SpootlightPictureExtractor/SpotlightPictureExtractor/ExtractImages.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Drawing;
+
+namespace SpotlightPictureExtractor
+{
+    class ExtractImages
+    {
+        public string pathToSource = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + 
+            @"\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"; //Path to Spotlight pictures
+        public string pathToDestination = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Spootlight Wallpapers"; //Desktop folder for wallpapers
+
+        public void DirectoryCheck()
+        {
+            if (!Directory.Exists(pathToSource) || (bool)!Directory.EnumerateFileSystemEntries(pathToSource).Any())
+            {
+                Console.WriteLine("Source folder doesn't exist or empty, check your Spotlight settings. Press any key for exit");
+                Console.ReadKey();
+                throw new Exception ("Source folder doesn't exist or empty, check your Spotlight settings."); //Stop the program if Spotlight is switched off
+            }
+
+            if (!Directory.Exists(pathToDestination))
+            {
+                Directory.CreateDirectory(pathToDestination); //Create directory on Desktop
+            }
+        }
+
+        public void CopyFiles()
+        {
+            DirectoryInfo di = new DirectoryInfo(pathToSource);
+            FileInfo[] files = di.GetFiles();
+            int fileName = Properties.Settings.Default.FileName;//File naming is set up in settings. Numbers are used as file names
+            Image tempImage;
+            
+            foreach (FileInfo file in files)
+            {
+                tempImage = Image.FromFile(file.DirectoryName + "\\" + file.Name);
+                if (tempImage.Width == 1920 && tempImage.Height == 1080) //Only FullHD wallpapers
+                {
+                    file.CopyTo(pathToDestination + "\\" + fileName + ".jpg"); //Rename files and add extensions
+                    fileName++;
+                }
+            }
+
+            Properties.Settings.Default.FileName = fileName;
+            Properties.Settings.Default.Save();
+        }
+
+
+    }
+}
---
+++ Spotlight/SpootlightPictureExtractor/SpotlightPictureExtractor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SpotlightPictureExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtractImages ei = new ExtractImages();
+            
+            ei.DirectoryCheck(); //Check whether directory exists or not 
+            ei.CopyFiles(); //Copy and modify wallpapers
+        }
+    }
+}
